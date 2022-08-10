b00d7701cc9e382dac1c75cb88a39f58584c86d1
panktip23 <106564804+panktip23@users.noreply.github.com>
2022-08-10 14:24:14 -0400

    Add files via upload

---
+++ FlaskApp.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pickle
+import pandas as pd
+from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+pickle_in = open("classifier.pkl", "rb")
+classifier = pickle.load(pickle_in)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [x for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = classifier.predict(final_features)
+
+    return render_template('index.html', prediction_text='The wheat seed belong to type {}'.format(prediction))
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
